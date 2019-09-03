9bfbb66aea75cc347ad93329ab6221a40bc7eae0
Diana Stefan <stefandianamihaela17@gmail.com>
2019-09-03 20:48:12 +0300

    added go to top button and functionality

--- js/common.js
+++ js/common.js
@@ -1,4 +1,19 @@
+//Scroll to top Button
+window.onscroll = function() {scrollFunction()};
 
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    document.getElementById("btnScrollToTop").style.display = "block";
+  } else {
+    document.getElementById("btnScrollToTop").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+////////////////////////////////////////////////////////////////////////
 
 // function setCopyrightDate(){
 //   year=new Date().getYear();
@@ -17,20 +32,3 @@
 // }
 //
 // getDate();
-
-// When the user scrolls down 20px from the top of the document, show the button
-// window.onscroll = function() {scrollFunction()};
-//
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     document.getElementById("myBtn").style.display = "block";
-//   } else {
-//     document.getElementById("myBtn").style.display = "none";
-//   }
-// }
-//
-// // When the user clicks on the button, scroll to the top of the document
-// function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
-//   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-// }
