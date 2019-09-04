fca598b34da35070faf8dbf62238511030cf74b2
Diana Stefan <stefandianamihaela17@gmail.com>
2019-09-04 14:49:35 +0300

    code review improvements

--- js/common.js
+++ js/common.js
@@ -2,10 +2,11 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+  var upButton = document.getElementById("btnScrollToTop");
   if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
-    document.getElementById("btnScrollToTop").style.display = "block";
+    upButton.style.display = "block";
   } else {
-    document.getElementById("btnScrollToTop").style.display = "none";
+    upButton.style.display = "none";
   }
 }
 
@@ -13,7 +14,7 @@ function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-////////////////////////////////////////////////////////////////////////
+
 
 // function setCopyrightDate(){
 //   year=new Date().getYear();
